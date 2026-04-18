605f88532fb185d2c26bea3a5df9576a42905544
wslider <wslider2000@gmail.com>
2026-04-18 12:00:11 -0400

    saved null agent values as -1

--- hotel_bookings.py
+++ hotel_bookings.py
@@ -12,3 +12,15 @@ hotel_bookings = hotel_bookings.rename(columns={
 
 hotel_bookings.to_csv('hotel_bookings.csv', index=False)
 
+# % of rows with null values
+print(hotel_bookings.isnull().sum() *100 / len(hotel_bookings))
+
+print (hotel_bookings['agent'].unique())
+print (hotel_bookings[hotel_bookings['agent'] == 5])
+
+# fill null agent entries with -1
+hotel_bookings['agent'] = hotel_bookings['agent'].fillna(-1)
+hotel_bookings.to_csv('hotel_bookings.csv', index=False)
+
+print(hotel_bookings[hotel_bookings['agent'] == -1].head())
+
